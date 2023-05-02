b0c8cb47266658df653a9576c3950dc2ebf19e06
joaoadavid <joao.antoniodavid@hotmail.com>
2023-05-02 13:46:27 -0300

    Starting to implament Board and Piece

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,5 +1,35 @@
 package boardgame;
 
 public class Board {// tabuleiro
+	
+	private int rows; // linhas do tabuleiro
+	private int columns; // coluna do tabuleiro
+	private Piece [][] pieces; //matriz de peças
+	
+	public Board(int rows, int columns) {
+		this.rows = rows;
+		this.columns = columns;
+		pieces = new Piece [rows][columns];
+	}
+
+	public int getRows() {
+		return rows;
+	}
+
+	public void setRows(int rows) {
+		this.rows = rows;
+	}
+
+	public int getColumns() {
+		return columns;
+	}
+
+	public void setColumns(int columns) {
+		this.columns = columns;
+	}
+	
+	
+	
+	
 
 }
