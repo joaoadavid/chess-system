678b4f3a67f334183b73ce94a91171523dd3ebc9
joaoadavid <joao.antoniodavid@hotmail.com>
2023-05-05 12:01:43 -0300

    chess layer and printing the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -28,7 +28,14 @@ public class Board {// tabuleiro
 		this.columns = columns;
 	}
 	
+	public Piece piece (int row, int column) { //retorna a matriz peça na coluna e na linha
+		
+		return pieces [row][column];
+	}
 	
+	public Piece piece (Position position) {
+		return pieces [position.getRow()][position.getColumn()];
+	}
 	
 	
 
